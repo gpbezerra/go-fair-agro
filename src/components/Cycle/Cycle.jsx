395c0ff293b61eb003f31e89afbb2a01992f015f
gpbezerra <gabrielpbezerra8@gmail.com>
2023-05-10 20:34:35 -0300

    insert hover, links and buttons

--- src/components/Cycle/Cycle.jsx
+++ src/components/Cycle/Cycle.jsx
@@ -17,25 +17,28 @@ export const Cycle = () => {
                         <p>Infraestrutura de Repositórios com Fátima Nunes Marques, Professora titular da Universidade de São Paulo.</p>
                         <img src={oficina1} alt="" />
                         <a href="https://www.youtube.com/watch?v=OyNUByzgP4A">Assistir oficina</a>
+                        <a href="https://miro.com/app/board/uXjVPfr9Uw4=/">Acesse o Miro</a>
                         <a id="cycleDrive" href="https://drive.google.com/file/d/17ZJ2bnJst_LMiQOD4TvzuDuEnSRKTyKp/view?usp=share_link">Acessar a apresentação</a>
                     </div>
                     <div className="cycleCard">
                         <p>Padrões de Dados e Metadados. Tipologia de Dados com Dr. João Luiz Moreira, Professor Assistente da Services and Cyber-Security (SCS).</p>
                         <img src={oficina2} alt="" />
                         <a href="https://www.youtube.com/watch?v=QmvO9zk-vPs">Assistir oficina</a>
+                        <a href="https://miro.com/app/board/uXjVPeZH5w4=/">Acesse o Miro</a>
                         <a id="cycleDrive" href="https://drive.google.com/file/d/1xNgCyOL1FiqyKQrxXADMEipAjuj6vtSA/view?usp=share_link">Acessar a apresentação</a>
-
                     </div>
                     <div className="cycleCard">
                         <p>Palestra Ontologia, ontologias e o “I” de FAIR com Giancarlo Guizzardi, Full Professor da Universidade de Twente.</p>
                         <img src={oficina3} alt="" />
                         <a href="https://www.youtube.com/watch?v=B_NbXSJWxgc">Assistir oficina</a>
+                        <a href="https://miro.com/app/board/uXjVPdTesSE=/">Acesse o Miro</a>
                         <a id="cycleDrive" href="https://drive.google.com/file/d/1BfPt-6VSzO_iP18-armDZCPduSc1nSRI/view?usp=share_link">Acessar a apresentação</a>
                     </div>
                     <div className="cycleCard">
                         <p>Política e Plano de Gestão de Dados, Viviane Veiga</p>
                         <img src={oficina4} alt="" />
-                        <a href="https://drive.google.com/file/d/1CIDrdIsyWbeqG3VxrlyBoZOOB1SZNJuz/view?usp=share_link">Assistir oficina</a>
+                        <a href="https://miro.com/app/board/uXjVPdTesSE=/">Acesse o Miro</a>
+                        <a href="https://drive.google.com/file/d/1CIDrdIsyWbeqG3VxrlyBoZOOB1SZNJuz/view?usp=share_link">Acessar a apresentação</a>
                     </div>
                 </div>
             </div>
--- src/components/Implementation/Implementation.jsx
+++ src/components/Implementation/Implementation.jsx
@@ -4,7 +4,7 @@ export const Implementation = () => {
     return(
         <div id="Implementação" className="videoContainer">
             <h1>Implementação</h1>
-            <p>A rede de implementação temática GO FAIR Agro é responsável por elaborar estratégias e mobilizar parcerias para a implantação dos princípios FAIR no universo dos dados da pesquisa agropecuária brasileira. O primeiro passo para a implantação da rede foi dado em novembro de 2021, durante o XIII Congresso Brasileiro de Agroinformática, com a elaboração do manifesto e a adesão de diversos atores. Em 2022, a rede está se organizando e conta com a participação da comunidade interessada pelo tema para a construção coletiva de sua dinâmica de trabalho. O evento de lançamento e 1a Oficina de trabalho da Rede GO FAIR Agro Brasil ocorreu em 12 de abril de 2022, no formato virtual, que contou com a apresentação do Professor Doutor Serra com o tema de GO FAIR Agro Brasil, Perspectivas e Desafios</p>
+            <p>A rede de implementação temática GO FAIR Agro é responsável por elaborar estratégias e mobilizar parcerias para a implantação dos princípios FAIR no universo dos dados da pesquisa agropecuária brasileira. O primeiro passo para a implantação da rede foi dado em novembro de 2021, durante o XIII Congresso Brasileiro de Agroinformática, com a elaboração do manifesto e a adesão de diversos atores. Em 2022, a rede está se organizando e conta com a participação da comunidade interessada pelo tema para a construção coletiva de sua dinâmica de trabalho. O evento de lançamento e 1ª Oficina de trabalho da Rede GO FAIR Agro Brasil ocorreu em 12 de abril de 2022, no formato virtual, que contou com a apresentação do Dr. Sergio Serra com o tema de GO FAIR Agro Brasil, Perspectivas e Desafios.</p>
             <div className="backgroundVideo">
                 <iframe
                     id="videoYtb"
@@ -18,14 +18,15 @@ export const Implementation = () => {
             <div className="implementationCardDeck">
                 <div id="implementationGP" className="implementationCard">
                     <ul>
-                        <li>Padrões de metadados e interoperabilidade</li>
-                        <li>Planos de gestão de dados</li>
-                        <li>Políticas de acesso e utilização de dados</li>
+                        <p>Grupos de trabalhos: </p>
+                        <li>GT Dados, Metadados e Repositórios</li>
+                        <li>GT Políticas e Planos de Gestão de Dados</li>
+                        <li>GT Ontologias</li>
                     </ul>
                 </div>
                 <div id="implementationMaterial" className="implementationCard">
-                    <p>Apresentação  GO FAIR Agro Brasil, Perspectivas e Desafios do Professor Doutor Serra</p>
-                    <a id="buttonDefault" href="https://drive.google.com/file/d/1GJJClgQgGCBiZJwVlq4UJiJs92dr_99j/view?usp=share_link">Acesse</a>
+                    <p>Apresentação  GO FAIR Agro Brasil, Perspectivas e Desafios do Professor Dr.Serra</p>
+                    <a id="buttonDefault" href="https://drive.google.com/file/d/1FaAJtmacb7ZwP8j1-xJT9uRLKiQDyBy7/view?usp=share_link">Acesse</a>
                 </div>
                 <div id="implementationOficina" className="implementationCard">
                     <p>Confira os resultados das oficinas serviram de base para a formação dos grupos de trabalho que vão atuar na construção coletiva dos planos de ação da rede.</p>
